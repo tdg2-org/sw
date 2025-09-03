5036b2673af5d037228783e58dbb68417d328d01
Tony <tonydgoodwin@gmail.com>
2025-09-03 12:59:38 -0400

    emmc partial bit load

---
+++ src/zub/dfx_bitsream_load/emmc_from_sd/README.md
@@ -0,0 +1,5 @@
+Copy bin files from SD to eMMC.
+
+partial bitstream load from eMMC.
+
+Fixed filenames and lengths. Meant as example.
---
+++ src/zub/dfx_bitsream_load/emmc_from_sd/xilffs_func_adg.h
@@ -0,0 +1,24 @@
+
+#include "xparameters.h"	/* SDK generated parameters */
+#include "xsdps.h"		/* SD device driver */
+#include "xil_printf.h"
+#include "ff.h"
+#include "xil_cache.h"
+#include "xplatform_info.h"
+
+#define RM0_LEN 135248
+#define RM1_LEN 287912
+
+
+extern u8 DestinationAddress[RM0_LEN];
+extern u8 DestinationAddress2[RM1_LEN];
+
+
+int run_SD_read(void);
+int prnt_vol_info(int SDx);//0 or 1. SD0 = eMMC, SD1 = SD card
+void list_dir(const char *path);
+int mountVol(int SDx);
+int umountVol(int SDx);
+int delFile(const TCHAR *filename);
+int copy_partials_sd1_to_emmc0(void);
+int read_emmc(void);
